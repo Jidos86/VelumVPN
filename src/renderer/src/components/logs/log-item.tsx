484abde4b41d7370d44cacbf0f28f482789b5052
coolcoala <pashokrulit@yandex.ru>
2026-03-05 10:29:15 +0300

    fixed flags in logs

--- src/renderer/src/components/logs/log-item.tsx
+++ src/renderer/src/components/logs/log-item.tsx
@@ -13,12 +13,12 @@ const LogItem: React.FC<ControllerLog & { index: number }> = (props) => {
     <div className={`px-2 pb-2 ${index === 0 ? 'pt-2' : ''}`}>
       <Card className="gap-0 py-0">
         <CardHeader className="pb-0 pt-1 px-3 gap-1">
-          <div className={`mr-2 text-lg flag-emoji font-bold ${colorMap[type]}`}>
+          <div className={`mr-2 text-lg font-bold ${colorMap[type]}`}>
             {props.type.toUpperCase()}
           </div>
           <small className="text-muted-foreground">{time}</small>
         </CardHeader>
-        <CardContent className="select-text pt-0 text-sm px-3 pb-2">{payload}</CardContent>
+        <CardContent className="select-text flag-emoji pt-0 text-sm px-3 pb-2">{payload}</CardContent>
       </Card>
     </div>
   )
